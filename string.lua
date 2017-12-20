834ad9f65be3cb80bb38a8f533e9e93d30da1593
ShanmugaPriya <priya97a@gmail.com>
2017-12-20 19:28:03 +1000

    strings

---
+++ string.lua
@@ -0,0 +1,11 @@
+quote ="Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+io.write("length ",string.len(quote),'\n')
+
+io.write('Replace Loreum with I ',string.gsub(quote,"Lorem","I"),'\n')
+
+io.write('index of elit ',string.find(quote,"elit"),'\n')
+
+io.write('UPPER CASE ',string.upper(quote),'\n')
+
+io.write('lower case',string.lower(quote),'\n')
